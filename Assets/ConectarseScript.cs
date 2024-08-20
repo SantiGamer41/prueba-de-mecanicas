116d159f39b0ba45583fe27871155015b5a6669a
valentinosanchez518 <141331384+valentinosanchez518@users.noreply.github.com>
2024-08-19 21:00:10 -0300

    Usuario se puede conectar al servidor multijugador

---
+++ Assets/ConectarseScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class ConectarseScript : MonoBehaviourPunCallbacks
+{
+    public InputField usernameInput;
+    public Text buttonText;
+
+    public void ClickConectarse()
+    {
+        if (usernameInput.text.Length >= 1)
+        {
+            PhotonNetwork.NickName = usernameInput.text;
+            buttonText.text = "Conectandose...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        SceneManager.LoadScene("LobbyMultijugador");
+    }
+}
