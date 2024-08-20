c6e55d3b75d098688f34dea211ec3e4a473fe823
valentinosanchez518 <141331384+valentinosanchez518@users.noreply.github.com>
2024-08-19 22:18:23 -0300

    Lobby hecha, faltan las salas en display

--- Assets/LobbyManager.cs
+++ Assets/LobbyManager.cs
@@ -2,18 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class LobbyManager : MonoBehaviour
+public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    public InputField roomInputField;
+    public GameObject lobbyPanel;
+    public GameObject roomPanel;
+    public Text nombreRoom;
+    public Text nombreBoton;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        PhotonNetwork.JoinLobby();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ClickCrear()
+    {
+        if(roomInputField.text.Length >= 1)
+        {
+            PhotonNetwork.CreateRoom(roomInputField.text);
+            nombreBoton.text = "Creando Sala...";
+        }
+        
+    }
+    public override void OnJoinedRoom()
+    {
+        lobbyPanel.SetActive(false);
+        roomPanel.SetActive(true);
+        nombreRoom.text = "Nombre de la sala: " + PhotonNetwork.CurrentRoom.Name;
     }
 }
