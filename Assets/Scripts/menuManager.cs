1ce406d638550ab1acf513d99330f4dc42a818dc
Manuchab <47963470@est.ort.edu.ar>
2024-11-05 10:55:05 -0300

    Chab's Commit 11: Más Chab, Más Commit

--- Assets/Scripts/menuManager.cs
+++ Assets/Scripts/menuManager.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class menuManager : MonoBehaviour
 {
     public AudioClip menuMusic;
     public Animator anim;
+    public GameObject panel;
+    public GameObject creditos;
+
    
 
     // Start is called before the first frame update
@@ -20,7 +24,15 @@ public class menuManager : MonoBehaviour
     {
         
     }
-
+    public void OnCreditsClick()
+    {
+        StartCoroutine(LoadPanel(creditos));
+    }
+    public void OnVolverClick()
+    {
+        Debug.Log("volvido");
+        StartCoroutine(LoadPanel(panel));
+    }
     public void OnClick()
     {
         StartCoroutine(LoadScene("InicioMultijugador"));
@@ -31,5 +43,18 @@ public class menuManager : MonoBehaviour
         anim.SetTrigger("endscene");
         yield return new WaitForSeconds(1.1f);
          SceneManager.LoadScene(scene);
+    }
+
+    private IEnumerator LoadPanel(GameObject panel)
+    {
+        anim.SetTrigger("endscene");
+        yield return new WaitForSeconds(1.1f);
+
+        // Desactivar todos los paneles primero
+        if (panel != null) panel.SetActive(false);
+        if (creditos != null) creditos.SetActive(false);
+
+        // Activar el panel que se desea mostrar
+        panel.SetActive(true);
     }
 }
